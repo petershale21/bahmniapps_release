954d44089904ab17b7a6d0db3f6bdb41c86e28fd
PhendukaHlompho <hlomphophenduka95@gmail.com>
2021-11-08 08:26:21 +0200

    Correction on the bahmniapps, update AHD regimen with Auto generate ID key on consultation

--- ui/app/common/concept-set/directives/conceptSet.js
+++ ui/app/common/concept-set/directives/conceptSet.js
@@ -308,7 +308,6 @@ angular.module('bahmni.common.conceptSet')
                             appService.setFormName($scope.conceptSetName);
     
                             autoFillFormValues(flattenedObs,fields);
-    
                         }
                             
     
@@ -341,7 +340,7 @@ angular.module('bahmni.common.conceptSet')
                              setObservationState(matchingObsArray, disable, error, hide, obsValue,conceptNaming,autocalculatevalue);
                             var obsTreatment = $scope.observations[0].groupMembers[0].groupMembers;
 
-                            //hack to check changes on ART treatment followup form to autopopulate medication from obs to medication tab --pheko---phenduka
+                            //hack to check changes on ART & AHD treatment followup form to autopopulate medication from obs to medication tab --pheko---phenduka
                             $scope.$watch(function() { 
                                 matchingObsArray.forEach(switchRegimen => {
                                     if(switchRegimen.label =="Name of Regimen Switched to") {
@@ -377,6 +376,18 @@ angular.module('bahmni.common.conceptSet')
                                             }
                                         }
                                     }
+
+                                    element.groupMembers.forEach(element => {
+                                        if(element.label == "Crypto Menengitis Regimen")
+                                        {
+                                            if(element.value != undefined ) {
+                                                appService.set_AHD_Regimen(element.value.displayString);
+                                            }
+                                            else{
+                                                appService.set_AHD_Regimen('');
+                                            }
+                                        }
+                                    });
                                 })
                             });
                             
